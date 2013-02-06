e16d6685e3b85f84dca871dc882dbcdc76ce1dca
koca <koca@koca-VirtualBox>
2013-02-06 22:37:46 +0100

    Drugi commit.

--- HitchStop/src/com/stop/hitch/MainActivity.java
+++ HitchStop/src/com/stop/hitch/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 //Komentar
+	//Drugi komentar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
