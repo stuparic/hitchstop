ff6a99b3158208e247cb9be275580d4bb2cf6f1c
koca <koca@koca-VirtualBox>
2013-02-12 00:18:45 +0100

    Slika pretrage

--- HitchStop/gen/com/stop/hitch/R.java
+++ HitchStop/gen/com/stop/hitch/R.java
@@ -14,18 +14,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int EditText01=0x7f070001;
+        public static final int TextView01=0x7f070000;
+        public static final int editText1=0x7f070002;
+        public static final int editText2=0x7f070003;
+        public static final int endButton=0x7f070007;
+        public static final int homeButton=0x7f070006;
+        public static final int menu_settings=0x7f070008;
+        public static final int nextButton=0x7f070004;
+        public static final int startButton=0x7f070005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_pretraga=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
+        public static final int activity_pretraga=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int cilj=0x7f040006;
+        public static final int datum=0x7f040007;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int polaziste=0x7f040004;
+        public static final int pretraga=0x7f040005;
+        public static final int title_activity_pretraga=0x7f040003;
     }
     public static final class style {
         /** 
@@ -54,5 +69,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int styleName=0x7f050002;
     }
 }
