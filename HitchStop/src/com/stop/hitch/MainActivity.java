b497f98a038221d956e724161a7ccb4cc7371338
ne0 <stuparic.dejan@gmail.com>
2013-02-13 00:41:51 +0100

    added link for enter route activity on main activity

--- HitchStop/src/com/stop/hitch/MainActivity.java
+++ HitchStop/src/com/stop/hitch/MainActivity.java
@@ -2,7 +2,10 @@ package com.stop.hitch;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = (Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent intent = new Intent(MainActivity.this, EnterRouteActivity.class);
+                startActivity(intent);
+            }
+        });
+        
     }
 
     @Override
@@ -19,4 +30,5 @@ public class MainActivity extends Activity {
         return true;
     }
     
+
 }
